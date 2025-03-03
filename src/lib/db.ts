c9ee86e042fb1a452d2ead91c08caaa3b5626554
Devansh Mayatra <devanshm667@gmail.com>
2025-03-03 15:32:17 +0530

    solved errors

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const MONGODB_URI = process.env.MONGODB_URI as string;
-    const DB_NAME = process.env.DB_NAME as String;
+    const DB_NAME = process.env.DB_NAME as string;
 
     const connectionInstance = await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`);
     console.log(`\nMongoDB Connected: ${connectionInstance.connection.host}`);
