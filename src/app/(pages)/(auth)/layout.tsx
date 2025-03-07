4ea643d2d663bbb3f3da3d117ed182a1219e823b
Devansh Mayatra <devanshm667@gmail.com>
2025-03-07 14:14:43 +0530

    updeted ui

--- src/app/(pages)/(auth)/layout.tsx
+++ src/app/(pages)/(auth)/layout.tsx
@@ -1,7 +1,14 @@
+import { Card, CardHeader } from "@/components/ui/card";
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
+    <div className="flex min-h-screen lg:gap-10 flex-col items-center lg:pt-10 bg-gray-100">
+      <Card className="m-4">
+        <CardHeader className="text-center lg:text-2xl">
+          Welcome yo yout Personal Transaction Visualizer
+        </CardHeader>
+      </Card>
+      <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-lg m-2">
         {children}
       </div>
     </div>
